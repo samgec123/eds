2897227c54378c1da89d94f5fd72bc515536e00d
Sameer Bhalerao <sbhalera@adobe.com>
2025-02-28 19:05:26 +0530

    Add fetchPlaceholders function to carousel block and log placeholder value; update paths.json to include placeholders mapping

--- blocks/carousels/carousels.js
+++ blocks/carousels/carousels.js
@@ -1,7 +1,9 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture, fetchPlaceholders } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 /* this is carousel block */
 export default function decorate(block) {
+  let placeholder = fetchPlaceholders();
+  console.log(placeholder['myKey']);
   const carouselId = 'carouselExampleCaptions';
   const carousel = document.createElement('div');
   carousel.id = carouselId;
